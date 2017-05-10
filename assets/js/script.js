402d832c0f93e584524b5b8bbaacc21878ec9db7
rplasschaert <robert.nl@hotmail.nl>
2017-05-10 12:20:29 +0200

    -

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,48 @@
 /**
  * Created by Robert on 27-4-2017.
  */
+var lastAnimalClicked = "";
+var image = "";
+
 
 (function() {
     $('#previous').click(function(){
         window.history.back();
     });
+    $('.animal').on('click', function(){
+        lastAnimalClicked = $(this).attr('id');
+        console.log('lastAnimalClicked: ' + lastAnimalClicked);
+    })
 })();
 
-function placeLion(latLng, map){
-    var image = "assets/media/lion.png";
-    var marker = new google.maps.Marker({
-        position: latLng,
-        map: map,
-        icon: image
-    });
+
+function placeAnimal(latLng, map){
+    switch(lastAnimalClicked){
+        case "lion":
+            image = "assets/media/lion-icon.png";
+            break;
+        case "elephant":
+            image = "assets/media/elephant-icon.png";
+            break;
+        case "rhino":
+            image = "assets/media/rhino-icon.png";
+            break;
+        case "leopard":
+            image = "assets/media/leopard-icon.png";
+            break;
+    }
+
+
+    if(lastAnimalClicked != ""){
+        var marker = new google.maps.Marker({
+            position: latLng,
+            map: map,
+            icon: image
+        });
+        lastAnimalClicked = "";
+    }
+
+
+
 
 }
